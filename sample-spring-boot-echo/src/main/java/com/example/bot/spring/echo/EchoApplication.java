4974511e9b743307f2813053f8569765c1f2e05d
kevin <k19891130@gmail.com>
2017-04-25 18:35:48 +0800

    dd

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -72,7 +72,7 @@ public class EchoApplication {
 		} else if(event.getMessage().getText().contains("學:")){
 			hmap.put(event.getMessage().getText().split("學:")[1].split("#31#")[0], event.getMessage().getText().split("學:")[1].split("#31#")[1]);
 			return new TextMessage("我學起來了。");
-		} else if(event.getMessage().getText().contains("$$") && event.getMessage().getText().length() == 6) {
+		} else if(event.getMessage().getText().contains("$$")) {
 			//String code = event.getMessage().getText().replace("$", "");
 			//String urlString = "http://finance.google.com/finance/info?client=ig&q=" + code;
 			String result = "查不到此股票";
