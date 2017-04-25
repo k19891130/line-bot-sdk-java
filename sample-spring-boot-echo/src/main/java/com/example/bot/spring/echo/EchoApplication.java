1f8f2e5b0a532e84c0cdc2c4680c45757df0dde1
kevin <k19891130@gmail.com>
2017-04-25 18:11:14 +0800

    8

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -81,45 +81,10 @@ public class EchoApplication {
 			} else if(event.getMessage().getText().contains("學:")){
 				hmap.put(event.getMessage().getText().split("學:")[1].split("#31#")[0], event.getMessage().getText().split("學:")[1].split("#31#")[1]);
 				return new TextMessage("我學起來了。");
-			} else if(event.getMessage().getText().contains("$$") && event.getMessage().getText().length() == 6) {
-				String code = event.getMessage().getText().replace("$", "");
-				String urlString = "http://finance.google.com/finance/info?client=ig&q=" + code;
-				String result = "查不到此股票";
-
-				try {
-					URL url = new URL(urlString);
-					HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-					connection.setRequestMethod("GET");
-					connection.setConnectTimeout(30000);
-					connection.setReadTimeout(30000);
-					connection.connect();
-					BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
-					StringBuilder response = new StringBuilder();
-					String line;
-					
-					while ((line = bufferedReader.readLine()) != null) {
-						response.append(line+"\n");
-					}
-					result = response.toString();
-					System.out.println(result);
-					String time = result.split("\"lt\" : \"")[1].split("\"")[0];
-					String money = result.split("\"l_cur\" : \"")[1].split("\"")[0];
-					String up = result.split("\"c\" : \"")[1].split("\"")[0];
-					result = "交易時間 : " + time + "\n價格 : " + money + "\n漲幅 : " + up;
-					return new TextMessage(result);
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					return new TextMessage("");
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					return new TextMessage("");
-				}
 			} else {
-				return new TextMessage("");
+				return null;
 			}
-			return new TextMessage("");
+			return null;
 		//} 
 		
 		//return new TextMessage("");
