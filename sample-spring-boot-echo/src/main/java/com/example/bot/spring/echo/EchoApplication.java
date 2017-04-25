cfb9570cac656a7c4e30550aad959c0d914b1bee
kevin <k19891130@gmail.com>
2017-04-25 18:20:52 +0800

    5

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -48,45 +48,64 @@ public class EchoApplication {
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);
-		
-		/*if(event.getMessage().getText().equals("Wanger出來")) {
-			power = true;
+
+		if(hmap.containsKey(event.getMessage().getText())) {
+			return new TextMessage(hmap.get(event.getMessage().getText()));
+		} else if(event.getMessage().getText().equals("早安")) {
+			return new TextMessage("早安吃飽了嘛?");
+		} else if(event.getMessage().getText().contains("愛我") && event.getMessage().getText().contains("?")) {
+			return new TextMessage("愛");
+		} else if(event.getMessage().getText().contains("美") && event.getMessage().getText().contains("?")) {
+			return new TextMessage("美");
+		} else if(event.getMessage().getText().contains("能") && event.getMessage().getText().contains("?")) {
+			return new TextMessage("能");
+		} else if(event.getMessage().getText().contains("帥") && event.getMessage().getText().contains("?")) {
+			return new TextMessage("帥");
+		} else if(event.getMessage().getText().contains("胖") && event.getMessage().getText().contains("?")) {
+			return new TextMessage("不胖");
+		} else if(event.getMessage().getText().contains("好") && event.getMessage().getText().contains("?")) {
 			return new TextMessage("好");
-		} else if(event.getMessage().getText().equals("Wanger掰")) {
-			power = false;
-			return new TextMessage("掰掰");
-		}*/
-		
-		//if(power) {
-			if(hmap.containsKey(event.getMessage().getText())) {
-				return new TextMessage(hmap.get(event.getMessage().getText()));
-			} else if(event.getMessage().getText().equals("早安")) {
-				return new TextMessage("早安吃飽了嘛?");
-			} else if(event.getMessage().getText().contains("愛我") && event.getMessage().getText().contains("?")) {
-				return new TextMessage("愛");
-			} else if(event.getMessage().getText().contains("美") && event.getMessage().getText().contains("?")) {
-				return new TextMessage("美");
-			} else if(event.getMessage().getText().contains("能") && event.getMessage().getText().contains("?")) {
-				return new TextMessage("能");
-			} else if(event.getMessage().getText().contains("帥") && event.getMessage().getText().contains("?")) {
-				return new TextMessage("帥");
-			} else if(event.getMessage().getText().contains("胖") && event.getMessage().getText().contains("?")) {
-				return new TextMessage("不胖");
-			} else if(event.getMessage().getText().contains("好") && event.getMessage().getText().contains("?")) {
-				return new TextMessage("好");
-			} else if(event.getMessage().getText().contains("對") && event.getMessage().getText().contains("?")) {
-				return new TextMessage("對");
-			} else if(event.getMessage().getText().contains("是") && event.getMessage().getText().contains("?")) {
-				return new TextMessage("是");
-			} else if(event.getMessage().getText().contains("學:")){
-				hmap.put(event.getMessage().getText().split("學:")[1].split("#31#")[0], event.getMessage().getText().split("學:")[1].split("#31#")[1]);
-				return new TextMessage("我學起來了。");
-			} else {
+		} else if(event.getMessage().getText().contains("對") && event.getMessage().getText().contains("?")) {
+			return new TextMessage("對");
+		} else if(event.getMessage().getText().contains("是") && event.getMessage().getText().contains("?")) {
+			return new TextMessage("是");
+		} else if(event.getMessage().getText().contains("學:")){
+			hmap.put(event.getMessage().getText().split("學:")[1].split("#31#")[0], event.getMessage().getText().split("學:")[1].split("#31#")[1]);
+			return new TextMessage("我學起來了。");
+		} else if(event.getMessage().getText().contains("$$") && event.getMessage().getText().length() == 6) {
+			String code = event.getMessage().getText().replace("$", "");
+			String urlString = "http://finance.google.com/finance/info?client=ig&q=" + code;
+			String result = "查不到此股票";
+			
+			try {
+				URL url = new URL(urlString);
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+				connection.setRequestMethod("GET");
+				connection.setConnectTimeout(30000);
+				connection.setReadTimeout(30000);
+				connection.connect();
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+				StringBuilder response = new StringBuilder();
+				String line;
+					
+				while ((line = bufferedReader.readLine()) != null) {
+					response.append(line+"\n");
+				}
+				result = response.toString();
+				System.out.println(result);
+				String time = result.split("\"lt\" : \"")[1].split("\"")[0];
+				String money = result.split("\"l_cur\" : \"")[1].split("\"")[0];
+				String up = result.split("\"c\" : \"")[1].split("\"")[0];
+				result = "交易時間 : " + time + "\n價格 : " + money + "\n漲幅 : " + up;
+				return new TextMessage(result);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 				return null;
 			}
-		//} 
-		
-		//return new TextMessage("");
+		} else {
+			return null;
+		}
     }
 
     @EventMapping
