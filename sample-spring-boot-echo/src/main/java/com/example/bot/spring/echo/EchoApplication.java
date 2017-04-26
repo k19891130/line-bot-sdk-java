7c06932b1d7281cda064f6c3a4be7e2c048caae0
kevin <k19891130@gmail.com>
2017-04-26 10:11:26 +0800

    9

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -72,8 +72,8 @@ public class EchoApplication {
 		} else if(event.getMessage().getText().contains("學:")){
 			hmap.put(event.getMessage().getText().split("學:")[1].split("#31#")[0], event.getMessage().getText().split("學:")[1].split("#31#")[1]);
 			return new TextMessage("我學起來了。");
-		} else if(event.getMessage().getText().contains("$$")) {
-			//String code = event.getMessage().getText().replace("$", "");
+		} else if(event.getMessage().getText().contains("$$")) { 
+			String code = event.getMessage().getText().replace("$", "");
 			//String urlString = "http://finance.google.com/finance/info?client=ig&q=" + code;
 			//String result = "查不到此股票";
 			
